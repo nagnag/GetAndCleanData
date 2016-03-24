f4c8c669ab747c86b206411e6728a33d261dbaaa
nagnag <nagesh.nagara@gmail.com>
2016-03-24 21:44:11 +0000

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,43 @@
+##########################################################################################################
+
+# run_analysis.R  
+
+# 
+#
+# This script creates a tidy data set from the study, "Smartphone-Based Human Activity Recognition." 
+#
+# Here is the link to the study -  http://www.tdx.cat/bitstream/handle/10803/284725/TJLRO1de1.pdf?sequence=1
+# 
+#  
+#
+# Required packages: plyr dplyr knitr memisc
+#
+# To arrive at a tidy data set, the following steps are performed as per instructions
+# 
+#
+# 0  Download the raw data
+#
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation for each measurement.
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive variable names.
+# 5. From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+##########################################################################################################
+
 ##
 ## preparatory steps
 ##
-library(data.table)
-library(reshape2)
+library(plyr)
+library(dplyr)
 library(knitr)
+library(memisc)
+
+#
+#  Replace dir as apporporiate
+#
 setwd("C:\\R\\CleanData\\Project")
 wd <- getwd()
+
 ##
 ## Download file
 ##
@@ -16,135 +48,127 @@ download.file(file_url, file.path(wd, file_name))
 ## Extract file from zip
 ##
 unzip(file_name)
-input_path <- paste0(wd,"\\UCI HAR Dataset")
+#
+#
 output_path <- paste0(wd,"\\output")
 list.files(input_path, recursive = TRUE)
-##
-## Read training  data set.
-##
 
-# subject
-data_Sub_Train <- fread(file.path(input_path, "train", "subject_train.txt"))
-data_Sub_Test <- fread(file.path(input_path, "test", "subject_test.txt"))
-# activity
-data_act_Train <- fread(file.path(input_path, "train", "subject_train.txt"))
-data_act_Test <- fread(file.path(input_path, "test", "subject_test.txt"))
+
+##########################################################################################################
+# Step 1: Merge the training and the test sets to create one data set.
+##########################################################################################################
+# 
+# Read the features file and assign to a data frame - 561 items
 #
-
-
-data_Train_f <- read.table(file.path(input_path, "train", "X_train.txt"))
-data_Train_tab <- data.table(data_Train_f)
-
-data_Test_f <- read.table(file.path(input_path, "test", "X_test.txt"))
-data_Test_tab <- data.table(data_Test_f)
-
-##
-## Merge the training and the test sets to create one data set.
-##
-
-##
-##   Merge Training and Test Subjects
-##
-
-data_Sub_tab <- rbind(data_Sub_Train, data_Sub_Test)
-setnames(data_Sub_tab, "V1", "subject")
-##
-##   Merge Training and Test Activities
-##
-data_Act_tab <- rbind(data_act_Train, data_act_Test)
-setnames(data_Act_tab, "V1", "activityNum")
-data_Merged_tab <- rbind(data_Train_tab, data_Test_tab)
-
-##
-##   Merge Columns
-##
-data_Sub_tab <- cbind(data_Sub_tab, data_Act_tab)
-data_Merged_tab <- cbind(data_Sub_tab, data_Merged_tab)
-#Set key
-
-setkey(data_Merged_tab, subject, activityNum)
-## Extracts only the measurements on the mean and standard deviation for each measurement.
-
-data_Features <- fread(file.path(input_path, "features.txt"))
-setnames(data_Features, names(data_Features), c("featureNum", "featureName"))
-data_Features <- data_Features[grepl("mean\\(\\)|std\\(\\)", featureName)]
-
-
-data_Features$featureCode <- data_Features[, paste0("V", featureNum)]
-head(data_Features)
+features_list <- read.table('UCI HAR Dataset/features.txt',header=FALSE, stringsAsFactors = FALSE); 
 #
-#Subset these variables using variable names.
+# Convert to lower case for consistency
 #
+features_list$V2 <- tolower(features_list$V2) 
 
-select <- c(key(data_Merged_tab), data_Features$featureCode)
-data_Merged_tab <- data_Merged_tab[, select, with = FALSE]
+# Read  Activity Labels from file:
+activity_labels <-read.table('UCI HAR Dataset/activity_labels.txt',header=FALSE);
 #
-# ## Uses descriptive activity names to name the activities in the data set ( based on activity_lables.txt)
+# Convert to lower case for consistency
 #
-data_ActNames <- fread(file.path(input_path, "activity_labels.txt"))
-setnames(data_ActNames, names(data_ActNames), c("activityNum", "activityName"))
-# Merge activity lables
-data_Merged_tab <- merge(data_Merged_tab, data_ActNames, by = "activityNum", all.x = TRUE)
+activity_labels$V2 <- tolower(activity_labels$V2)
+
+# Read activity ids for training
+train_activity_Id <- read.table('UCI HAR Dataset/train/Y_train.txt',header=FALSE) 
+
+# Read activity ids for test
+test_activity_Id <- read.table('UCI HAR Dataset/test/y_test.txt',header=FALSE)
+
+# Read file for observations per subject for training (21 subjects, 7352 total obs)
+Train_obs <-read.table('UCI HAR Dataset/train/subject_train.txt',header=FALSE) 
+
+# Read file for observations per subject for test (9 subjects, 2947 total obs) 
+Test_obs <- read.table('UCI HAR Dataset/test/subject_test.txt',header=FALSE)
+
+# Read in the training and  test data sets
+
+# Read in training data (called the 'Training set'in README.txt) into a data frame:
+Training_Set <- read.table('UCI HAR Dataset/train/X_train.txt',header=FALSE)
+
+# Read in test data (called the 'Test set'in README.txt) into a data frame:
+Test_Set <- read.table('UCI HAR Dataset/test/x_test.txt',header=FALSE)
+
+# Assign sensible variable names to all data sets
+colnames(Train_obs) <- "subject"
+colnames(Test_obs) <- "subject"
+colnames(Training_Set) <- features_list[,2]
+colnames(Test_Set) <- features_list$V2 # features[,2]
+colnames(train_activity_Id) <- "activityid";
+colnames(test_activity_Id) <- "activityid";
+
+# create full training and test data sets: append subject and activity_Id variables to x_train & x_test 
+Training_Set_c <- cbind(Training_Set,Train_obs,train_activity_Id)
+Test_Set_c <- cbind(Test_Set,Test_obs,test_activity_Id)
+
+# Merge test and train data sets
+merged_Data <- rbind(Training_Set_c,Test_Set_c)
+
+##########################################################################################################
+# Step 2: Extract only the measurements on the mean and standard deviation for each measurement
+# (but also include the activityid and subject variables, as I will need those later)
+##########################################################################################################
+
 #
-## Appropriately labels the data set with descriptive variable names.
+# Fetch column names
 #
-#Add activityName as a key.
+data_vars <- names(merged_Data)
 
-setkey(data_Merged_tab, subject, activityNum, activityName)
+# 53 mean variables
+mean_vars <- grep("mean",tolower(data_vars),value=TRUE)
+# 33 std variables
+std_vars <- grep("std",tolower(data_vars),value=TRUE)
 
-# Melt the data table to reshape it from a short and wide format to a tall and narrow format.
+# Store lables
+ActSub <- c("activityid","subject")
 
-data_Merged_tab <- data.table(melt(data_Merged_tab, key(data_Merged_tab), variable.name = "featureCode"))
+meanStd_fields <- c(mean_vars,std_vars,ActSub)
+meanStd_filtered <- merged_Data[,meanStd_fields]
 
-# Merge activity name.
+##########################################################################################################
+# Step 3. Use descriptive activity names to name the activities in the data set
+##########################################################################################################
+meanStd_filtered <- merge(meanStd_filtered,activity_labels,by.x="activityid",by.y="V1",all=TRUE)
 
-data_Merged_tab <- merge(data_Merged_tab, data_Features[, list(featureNum, featureCode, featureName)], by = "featureCode", 
-            all.x = TRUE)
+##########################################################################################################
+# Step 4. Appropriately label the data set with descriptive activity names. 
+##########################################################################################################
 
-#Appropriately labels the data set with descriptive variable names.
-# Create a new variable, activity that is equivalent to activityName as a factor class. Create a new variable, feature that is equivalent to featureName as a factor class.
+# get all the current column names
+meanStd_filterednames <- colnames(meanStd_filtered)
 
-data_Merged_tab$activity <- factor(data_Merged_tab$activityName)
-data_Merged_tab$feature <- factor(data_Merged_tab$featureName)
+# Rename the "V2" column name to the descriptive column name "activity"
+meanStd_filterednames[89] <- "activity"
 
+# Get rid of  "()" from column lable
+meanStd_filterednames <- sub("\\()","",meanStd_filterednames)
 
-match_feat <- function(expr) {
-  grepl(expr, data_Merged_tab$feature)
-}
-## Features with 2 categories
-n <- 2
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(match_feat("^t"), match_feat("^f")), ncol = nrow(y))
-data_Merged_tab$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
-x <- matrix(c(match_feat("Acc"), match_feat("Gyro")), ncol = nrow(y))
-data_Merged_tab$featInstrument <- factor(x %*% y, labels = c("Accelerometer", "Gyroscope"))
-x <- matrix(c(match_feat("BodyAcc"), match_feat("GravityAcc")), ncol = nrow(y))
-data_Merged_tab$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
-x <- matrix(c(match_feat("mean()"), match_feat("std()")), ncol = nrow(y))
-data_Merged_tab$featVariable <- factor(x %*% y, labels = c("Mean", "SD"))
-## Features with 1 category
-data_Merged_tab$featJerk <- factor(match_feat("Jerk"), labels = c(NA, "Jerk"))
-data_Merged_tab$featMagnitude <- factor(match_feat("Mag"), labels = c(NA, "Magnitude"))
-## Features with 3 categories
-n <- 3
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(match_feat("-X"), match_feat("-Y"), match_feat("-Z")), ncol = nrow(y))
-data_Merged_tab$featAxis <- factor(x %*% y, labels = c(NA, "X", "Y", "Z"))
+# reassign more descriptive column names
+colnames(meanStd_filtered) <- meanStd_filterednames
 
-# Check to make sure all possible combinations of feature are accounted for by all possible combinations of the factor class variables.
-#
-r1 <- nrow(data_Merged_tab[, .N, by = c("feature")])
-r2 <- nrow(data_Merged_tab[, .N, by = c("featDomain", "featAcceleration", "featInstrument", 
-                           "featJerk", "featMagnitude", "featVariable", "featAxis")])
-r1 == r2
+##########################################################################################################
+# Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable 
+#    for each activity and each subject.
+##########################################################################################################
 
-## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# remove 'activitiyid' column
 
-setkey(data_Merged_tab, subject, activity, featDomain, featAcceleration, featInstrument, 
-       featJerk, featMagnitude, featVariable, featAxis)
-Tidy_data_Set <- data_Merged_tab[, list(count = .N, average = mean(value)), by = key(data_Merged_tab)]
+meanStd_filtered <- subset(meanStd_filtered, select=-c(activityid))
+
+Tidy_data_Set = ddply(meanStd_filtered, c("subject","activity"), numcolwise(mean))
+
 #
 # Write Output to file
 #
 f <- file.path(output_path, "ActivityMeasurementUsingSmartPhone.txt")
 write.table(Tidy_data_Set, f, quote = FALSE, sep = "\t", row.names = FALSE)
+#
+# Write Codebook to file
+#
+cf <- file.path(output_path, "Codebook.md")
+Write(codebook(Tidy_data_Set),
+      file=cf)
